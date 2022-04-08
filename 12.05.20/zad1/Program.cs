2fbf59cadd926bd34fe1d90f70bd883e6789c545
Ahmed Gavaz <93177410+ahmedgavaz@users.noreply.github.com>
2022-04-08 15:20:37 +0300

    Add files via upload

---
+++ 12.05.20/zad1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            while (a!=b)
+            { if (a > b) a = a - b;
+                if (b > a) b = b - a;
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ 13.05.20/zad2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zad2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 2;int p=1;
+            int n = int.Parse(textBox1.Text); 
+            while (i<=n)
+            {
+                p = p * i;
+                i = i + 2;     }
+            label1.Text = p.ToString();
+        }
+    }
+}
