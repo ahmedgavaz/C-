826bcb2116edc8ad6151df243fab92266dda750a
Ahmed Gavaz <93177410+ahmedgavaz@users.noreply.github.com>
2022-04-08 15:35:54 +0300

    Add files via upload

---
+++ 20.05.20/zad5/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zad5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics; 
+            Random r = new Random(); 
+            int h = 50, red, green, blue; 
+            for (int i = 1; i <= 10; i++)
+            {
+                int w = r.Next() % 20 + 10; 
+                red = r.Next() % 64;
+                green = r.Next() % 128; blue = r.Next() % 32;
+                Pen p = new Pen(Color.FromArgb(red, green, blue), w); 
+                g.DrawLine(p, 100, h, 400, h); h = h + w / 2;
+            }
+        }
+    }
+}
