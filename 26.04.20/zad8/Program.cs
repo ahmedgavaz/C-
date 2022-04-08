cc0e3e5de031d0c8417e1163fad04e3332c730a6
Ahmed Gavaz <93177410+ahmedgavaz@users.noreply.github.com>
2022-04-08 15:33:37 +0300

    Add files via upload

---
+++ 26.04.20/zad8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace zad8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double hp = double.Parse(Console.ReadLine());
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            int e = int.Parse(Console.ReadLine());
+            double s = a * 2.6 + b * 3 + c * 4.1 + d * 8.2 + e * 2;
+            if (a + b + c + d + e > 49) s = s * 0.75;
+            s = s * 0.9;
+            double x = Math.Abs(s - hp);
+            if (hp <= s) Console.WriteLine($"Yes! {x:f2} lv left.");
+            else Console.WriteLine($"Not enough money! {x:f2} lv needed.");
+        }
+    }
+}
