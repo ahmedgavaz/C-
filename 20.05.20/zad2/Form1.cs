bd515e1a5d302211cd51fa46839d15108a6720ff
Ahmed Gavaz <93177410+ahmedgavaz@users.noreply.github.com>
2022-04-08 15:35:18 +0300

    Add files via upload

---
+++ 20.05.20/zad2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zad2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+             protected override void OnPaint(PaintEventArgs e) 
+            { Graphics g = e.Graphics; 
+            Pen p = new Pen(Color.Black, 2); 
+            int side = 100, x1 = 0, y1 = 500, x2, y2, x3, y3; 
+            for (; x1 <= 800; x1 += 20, y1 -= 10) 
+            { x2 = x1 + side; y2 = y1; 
+                x3 = (x1 + x2) / 2;
+                int h = (int)(side * Math.Sqrt(3) / 2); 
+                y3 = y1 - h; g.DrawLine(p, x1, y1, x2, y2); 
+                g.DrawLine(p, x2, y2, x3, y3);
+                g.DrawLine(p, x3, y3, x1, y1); } }                                                                                                      
+    }
+}
---
+++ 20.05.20/zad3/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zad3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen p = new Pen(Color.Black, 2);
+            int side = 100, x1 = 0, y1 = 100, x2, y2, x3, y3;
+            for (; x1 <= 800; x1 += 20)
+            {
+                x2 = x1 + side; y2 = y1;
+                x3 = (x1 + x2) / 2;
+                int h = (int)(side * Math.Sqrt(3) / 2);
+                y3 = y1 - h;
+                g.DrawLine(p, x1, y1, x2, y2);
+                g.DrawLine(p, x2, y2, x3, y3);
+                g.DrawLine(p, x3, y3, x1, y1);
+            }
+        }
+    }
+}
